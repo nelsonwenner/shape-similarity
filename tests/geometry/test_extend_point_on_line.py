ea75cca006427024402657075b152a192b66fc24
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-14 16:46:05 -0300

    test: Should ensure be able return a point distance away from the end point

---
+++ tests/geometry/test_extend_point_on_line.py
@@ -0,0 +1,10 @@
+import unittest
+import src
+
+class TestExtendPointOnLine(unittest.TestCase):
+  def test_returns_a_point_distance_away_from_the_end_point(self):
+    point1, point2 = [0, 0], [8, 6]
+    self.assertEqual(
+      src.geometry.extend_point_on_line(point1, point2, 5), 
+      [12, 9]
+    )
