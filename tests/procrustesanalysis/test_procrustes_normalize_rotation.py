f081d8b56e50e0b7b325800a0bbde2f46dc886a2
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 14:22:11 -0300

    test: throws an error if the curves have different numbers of points

--- tests/procrustesanalysis/test_procrustes_normalize_rotation.py
+++ tests/procrustesanalysis/test_procrustes_normalize_rotation.py
@@ -11,3 +11,10 @@ class TestProcrustesNormalizeRotation(unittest.TestCase):
       list(map(lambda item: [round(item[0], 4), round(item[1], 4)], rotated_curve)),
       list(map(lambda item: [round(item[0], 4), round(item[1], 4)], rotated_curve))
     )
+
+  def test_throws_an_error_if_the_curves_have_different_numbers_of_points(self):
+    curve1 = [[0, 0], [1, 1]]
+    curve2 = [[0, 0], [1, 1], [1.5], 1.5]
+    with self.assertRaises(AssertionError) as context:
+      src.procrustesanalysis.procrustes_normalize_rotation(curve1, curve2)
+    self.assertTrue('curve and relativeCurve must have the same length' in str(context.exception))
