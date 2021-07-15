aadf872ae5697446f0ec5c1ddf2f16ad16eda706
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 12:08:11 -0300

    feat: add find_procrustes_rotation_angle

--- src/procrustesanalysis.py
+++ src/procrustesanalysis.py
@@ -23,3 +23,27 @@ def procrustes_normalize_curve(curve, rebalance=True, estimationPoints=50):
   translated_curve = list(map(lambda point: substract(point, mean), balanced_curve))
   scale = math.sqrt(array_average(list(map(lambda item: item[0]*item[0] + item[1]*item[1], translated_curve))))
   return list(map(lambda item: [item[0] / scale, item[1] / scale], translated_curve))
+
+'''
+from https://en.wikipedia.org/wiki/Procrustes_analysis
+'''
+def find_procrustes_rotation_angle(curve, relativeCurve):
+  '''
+  Args:
+    curve: type array [[x, y]], [x, y]].
+    relativeCurve: type array [[x, y]], [x, y]].
+  Warnings:
+    `curve` and `relativeCurve` must have the same number of points
+    `curve` and `relativeCurve` should both be run through [[procrustesNormalizeCurve]] first
+  Returns:
+    find_procrustes_rotation_angle: rotation_angle
+  Descriptions:
+    Find the angle to rotate `curve` to match the rotation
+    of `relativeCurve` using procrustes analysis
+  '''
+  assert len(curve) == len(relativeCurve), 'curve and relativeCurve must have the same length'
+  numerator, denominator = 0, 0
+  for i in range(0, len(curve)):
+    numerator += curve[i][1] * relativeCurve[i][0] - curve[i][0] * relativeCurve[i][1]
+    denominator += curve[i][0] * relativeCurve[i][0] + curve[i][1] * relativeCurve[i][1]
+  return math.atan2(numerator, denominator)
