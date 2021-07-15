117cb317c3c54db49512e0a186c44861ef2f19e1
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 12:14:13 -0300

    test: Should ensure be able return 0 if the curves have the same rotation

--- tests/procrustesanalysis/test_find_procrustes_rotation_angle.py
+++ tests/procrustesanalysis/test_find_procrustes_rotation_angle.py
@@ -10,3 +10,11 @@ class TestFindProcrustesRotationAngle(unittest.TestCase):
       src.procrustesanalysis.find_procrustes_rotation_angle(curve1, curve2),
       (-1 * math.pi) / 2
     )
+
+  def test_return_0_if_the_curves_have_the_same_rotation(self):
+    curve1 = [[0, 0], [1, 1]]
+    curve2 = [[0, 0], [1.5, 1.5]]
+    self.assertEqual(
+      src.procrustesanalysis.find_procrustes_rotation_angle(curve1, curve2),
+      0
+    )
