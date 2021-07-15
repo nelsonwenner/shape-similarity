2cf6386b999871b5de69e281e0d51dca536ba819
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 10:53:07 -0300

    chore: add commentaries

--- src/procrustesanalysis.py
+++ src/procrustesanalysis.py
@@ -6,6 +6,16 @@ import math
 from https://en.wikipedia.org/wiki/Procrustes_analysis
 '''
 def procrustes_normalize_curve(curve, rebalance=True, estimationPoints=50):
+  '''
+  Args:
+    curve: type array [[x, y]], [x, y]].
+    rebalance: Optionally runs default True
+    estimationPoints: Optionally runs default 50
+  Returns:
+    procrustes_normalize_curve: procrustes_normalize_curve
+  Descriptions:
+    Translate and scale curve by Procrustes Analysis
+  '''
   balanced_curve = rebalance_curve(curve, estimationPoints) if rebalance else curve
   mean_x = array_average(list(map(lambda item: item[0], balanced_curve)))
   mean_y = array_average(list(map(lambda item: item[1], balanced_curve)))
