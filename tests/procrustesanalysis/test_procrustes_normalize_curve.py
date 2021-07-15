ca957a0a0632a808575bdf750db364df50f6c1bf
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 10:41:36 -0300

    test: Should ensure be able normalizes the scale and translation of the curve

---
+++ tests/procrustesanalysis/test_procrustes_normalize_curve.py
@@ -0,0 +1,19 @@
+import unittest
+import math
+import src
+
+class TestProcrustesNormalizeCurve(unittest.TestCase):
+  def test_normalizes_the_scale_and_translation_of_the_curve(self):
+    curve = [[0, 0], [4, 4]]
+    result = src.procrustesanalysis.procrustes_normalize_curve(curve, rebalance=False)
+    self.assertEqual(
+      [
+        [round(result[0][0], 4), round(result[0][1], 4)],
+        [round(result[1][0], 4), round(result[1][1], 4)]
+      ],
+      [
+        [round((-1 * math.sqrt(2)) / 2, 4), round((-1 * math.sqrt(2)) / 2, 4)],
+        [round((math.sqrt(2)) / 2, 4), round((math.sqrt(2)) / 2, 4)]
+      ]
+    )
+  
