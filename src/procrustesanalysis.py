e0e9cc2ce0b121073857db5f61c239ef00bf270c
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-15 10:39:14 -0300

    feat: add procrustes_normalize_curve

---
+++ src/procrustesanalysis.py
@@ -0,0 +1,15 @@
+from .geometry import *
+from .utils import *
+import math
+
+'''
+from https://en.wikipedia.org/wiki/Procrustes_analysis
+'''
+def procrustes_normalize_curve(curve, rebalance=True, estimationPoints=50):
+  balanced_curve = rebalance_curve(curve, estimationPoints) if rebalance else curve
+  mean_x = array_average(list(map(lambda item: item[0], balanced_curve)))
+  mean_y = array_average(list(map(lambda item: item[1], balanced_curve)))
+  mean = [mean_x, mean_y]
+  translated_curve = list(map(lambda point: substract(point, mean), balanced_curve))
+  scale = math.sqrt(array_average(list(map(lambda item: item[0]*item[0] + item[1]*item[1], translated_curve))))
+  return list(map(lambda item: [item[0] / scale, item[1] / scale], translated_curve))
