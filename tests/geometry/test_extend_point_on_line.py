300352b256918c203f74b7cb5fa3aebcc3ecb82e
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-17 12:00:00 -0300

    refactor: improving logic

--- tests/geometry/test_extend_point_on_line.py
+++ tests/geometry/test_extend_point_on_line.py
@@ -6,33 +6,33 @@ class TestExtendPointOnLine(unittest.TestCase):
     point1, point2 = [0, 0], [8, 6]
     self.assertEqual(
       src.geometry.extend_point_on_line(point1, point2, 5), 
-      [12, 9]
+      [4.0, 3.0]
     )
   
   def test_works_with_negative_distances(self):
     point1, point2 = [0, 0], [8, 6]
     self.assertEqual(
       src.geometry.extend_point_on_line(point1, point2, -5), 
-      [4, 3]
+      [12.0, 9.0]
     )
 
   def test_works_when_p2_is_before_p1_in_the_line(self):
     point1, point2 = [12, 9], [8, 6]
     self.assertEqual(
       src.geometry.extend_point_on_line(point1, point2, 10), 
-      [0, 0]
+      [16.0, 12.0]
     )
 
   def test_works_with_vertical_lines(self):
     point1, point2 = [2, 4], [2, 6]
     self.assertEqual(
       src.geometry.extend_point_on_line(point1, point2, 7), 
-      [2, 13]
+      [2.0, -1.0]
     )
 
   def test_works_with_vertical_lines_where_p2_is_above_p1(self):
     point1, point2 = [2, 6], [2, 4]
     self.assertEqual(
       src.geometry.extend_point_on_line(point1, point2, 7), 
-      [2, -3]
+      [2.0, 11.0]
     )
