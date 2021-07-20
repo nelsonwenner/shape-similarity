6baa43228bb1e1e7e1167abb62275296eab7a728
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-19 22:17:38 -0300

    refactor: update info

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import io
+
+setup(
+  name='shapesimilarity',
+  version='0.0.1',
+  author='Nelson Wenner',
+  packages=['shapesimilarity'],
+  url='https://github.com/nelsonwenner/shape-similarity',
+  description='Quantify the similarity between two shapes/curves',
+  long_description_content_type="text/markdown",
+  long_description=io.open('README.md', encoding="utf-8").read(),
+  keywords=['python', 'curve', 'frechet-distance', 'procrustes-analysis'],
+  platforms=['any']
+)
