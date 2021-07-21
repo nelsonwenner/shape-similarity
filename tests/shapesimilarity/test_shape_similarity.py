191a0388f1715ad2af51b54267b84aa7761196d1
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-21 00:53:29 -0300

    test: should be really close to 0 for very dissimilar shape

--- tests/shapesimilarity/test_shape_similarity.py
+++ tests/shapesimilarity/test_shape_similarity.py
@@ -11,3 +11,8 @@ class TestShapeSimilarity(unittest.TestCase):
     curve1 = [[0, 0], [2, 4], [4, 0], [0, 0]]
     curve2 = [[0, 0], [2, 2], [2, 2], [1, 1]]
     self.assertEqual(shape_similarity(curve1, curve2), 0.4792)
+
+  def test_should_be_really_close_to_0_for_very_dissimilar_shapes(self):
+    curve1 = [[0, 0], [0.1, 0.1], [0.1, 0.1], [1, 1]]
+    curve2 = [[7, 7], [4, 4], [4, 4], [5, 5]]
+    self.assertEqual(shape_similarity(curve1, curve2), 0)
