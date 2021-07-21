ea73d444b1c5f80ffd62184d42a367507ef00bb2
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-21 15:42:57 -0300

    test: ensure be able to returns 0 for empty arrays

--- tests/utils/test_array_sum.py
+++ tests/utils/test_array_sum.py
@@ -4,3 +4,6 @@ import unittest
 class TestArraySum(unittest.TestCase):
   def test_returns_the_sum_of_all_elements_in_the_array(self):
     self.assertEqual(array_sum([1, 3, 5]), 9)
+
+  def test_returns_0_for_empty_arrays(self):
+    self.assertEqual(array_sum([]), 0)
