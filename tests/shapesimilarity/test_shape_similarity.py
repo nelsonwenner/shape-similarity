b37ed8af04a99ecd406857306c4b8d5e4fdb715a
wenner <nelsondiasdemedeiros@gmail.com>
2021-07-21 00:26:58 -0300

    test: ensure be able returns low numbers for curves with dissimilar shapes

--- tests/shapesimilarity/test_shape_similarity.py
+++ tests/shapesimilarity/test_shape_similarity.py
@@ -6,3 +6,8 @@ class TestShapeSimilarity(unittest.TestCase):
     curve1 = [[0, 0], [2, 4], [18, -3]]
     curve2 = [[0.3, -0.2], [2.2, 4.5], [16, -4]]
     self.assertEqual(shape_similarity(curve1, curve2), 0.9127)
+
+  def test_returns_low_numbers_for_curves_with_dissimilar_shapes(self):
+    curve1 = [[0, 0], [2, 4], [4, 0], [0, 0]]
+    curve2 = [[0, 0], [2, 2], [2, 2], [1, 1]]
+    self.assertEqual(shape_similarity(curve1, curve2), 0.4792)
